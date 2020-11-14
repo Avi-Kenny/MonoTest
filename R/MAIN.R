bf3bd330db0cffc9eaab566c333dfa6d3fd033e6
Avi Kenny <avikenny@uw.edu>
2020-11-13 17:36:50 -0800

    Misc updates

--- R/MAIN.R
+++ R/MAIN.R
@@ -4,14 +4,6 @@
 
 
 ################.
-##### INFO #####
-################.
-
-# !!!!! TO DO
-
-
-
-################.
 ##### SETUP #####
 ################.
 
@@ -42,32 +34,26 @@ if (Sys.getenv("USERDOMAIN")=="AVI-KENNY-T460") {
 
 
 
-##############################.
-##### MAIN: Density test #####
-##############################.
+############################################.
+##### MAIN: Simulation setup (density) #####
+############################################.
 
 if (run_main) {
   
   sim <- new_sim()
   
   sim %<>% set_config(
-    num_sim = 10000,
     parallel = "none",
     packages = c("magrittr", "dplyr")
   )
   
-  sim %<>% set_levels(
-    n = c(10,20,30,40,50),
-    test = c("Method 1 (K=1)", "Z test", "L test", "D test", "Slope"),
-    true_density = c("f(x)=1", "f(x)=2x","f(x)=ke^x")
-  )
-  
   # This function generates data
   #   `n` is the sample size
-  #   `true_density` is the type of density used to generate the data
+  #   `true_density` is the type of density used to generate the data, specified
+  #       as a character string
   sim %<>% add_creator("generate_dataset", function(n, true_density) {
     
-    # !!!!! Add beta distributions
+    # !!!!! Add other monotone distributions
     
     # Quantile function for probability integral transform sampling
     if (true_density=="f(x)=1") {
@@ -82,11 +68,43 @@ if (run_main) {
     
   })
   
-  sim %<>% add_method("Method 1 (K=1)", function(dat, n) {
-    as.numeric(sqrt(n)*(2-4*mean(dat<=0.5))>2*qnorm(0.95))
+  # Marco method 1
+  # Currently assumes equally sized intervals of size delta
+  # wts argument can either be "equal" or a vector of length (1/delta)-1
+  sim %<>% add_method("CumlIncr", function(dat, n, params) {
+    
+    delta <- params$delta
+    if (params$wts=="equal") {
+      wts <- rep(1/(1/delta-1),1/delta-1)
+    } else {
+      wts <- params$wts
+    }
+    
+    if (params$wts=="equal") {
+      
+      T_n <- 1 - mean(dat<=1-delta) - mean(dat<=delta)
+      asy_sd <- sqrt(2*delta)
+
+    } else if (delta==1/2) {
+      
+      # !!!!! This is technically the same test as above; combine
+      T_n <- 2 - 4*mean(dat<=0.5)
+      asy_sd <- 2
+
+    } else if (delta==1/3) {
+      
+      w1 <- wts[1]
+      w2 <- wts[2]
+      T_n <- 3*(w1-2*w2)*mean(dat<=2/3) + 3*(w2-2*w1)*mean(dat<=1/3) + 3*w2
+      asy_sd <- sqrt( 6*(w1^2-w1*w2+w2^2) )
+      
+    }
+    
+    return (as.numeric(sqrt(n)*T_n>asy_sd*qnorm(0.95)))
+    
   })
   
-  # !!!!! Generate beta_n distribution
+  # Generate beta_n distribution and add as a simulation constant
   beta_n_distr <- list()
   for (n in c(10,20,30,40,50)) {
     beta_ns <- c()
@@ -100,9 +118,9 @@ if (run_main) {
     }
     beta_n_distr[[as.character(n)]] <- beta_ns
   }
-  
   sim %<>% add_constant("beta_n_distr", beta_n_distr)
   
+  # Marco method 2
   sim %<>% add_method("Slope", function(dat, n) {
     Theta_hat <- ecdf(dat)
     mu_2n <- mean(dat^2)
@@ -123,7 +141,7 @@ if (run_main) {
   })
   
   # D test from Woodroofe and Sun 1999 (c=0.2)
-  # !!!!! Testing
+  # !!!!! Does not work yet; gives too high rejection rates
   sim %<>% add_method("D test", function(dat, n) {
     t <- seq(0,1,0.001)
     # t <- seq(0,1,0.01)
@@ -149,26 +167,164 @@ if (run_main) {
     
     beta_n_distr <- C$beta_n_distr
     dat <- generate_dataset(L$n, L$true_density)
-    reject <- do.call(L$test, list(dat, L$n))
+    if (is.null(L$test$params)) {
+      reject <- do.call(L$test$type, list(dat, L$n))
+    } else {
+      reject <- do.call(L$test$type, list(dat, L$n, L$test$params))
+    }
+    
     return (list("reject"=reject))
     
   })
   
-  sim %<>% run("one_simulation")
+}
+
+
+
+###################################################.
+##### MAIN: Sim #1: compare different methods #####
+###################################################.
+
+if (run_main) {
   
-  sim$constants <- NULL # !!!!! TEMP FIX
-  summ <- sim %>% summary() %>%
-    rename("power"=`mean_reject`) %>%
-    arrange(true_density, n, test)
+  sim_1 <- sim
+
+  sim_1 %<>% set_config(num_sim=10000)
   
+  sim_1 %<>% set_levels(
+    n = c(10,20,30,40,50),
+    test = list(
+      "Z test" = list(type="Z test"),
+      "L test" = list(type="L test"),
+      # "D test" = list(type="D test"),
+      "Slope" = list(type="Slope"),
+      "CumlIncr (delta=1/2)" = list(
+        type = "CumlIncr",
+        params = list(delta=(1/2), wts=1)
+      ),
+      "CumlIncr (delta=1/3, equal weights)" = list(
+        type = "CumlIncr",
+        params = list(delta=(1/3), wts=c(0.5,0.5))
+      )
+    ),
+    true_density = c("f(x)=1", "f(x)=2x", "f(x)=ke^x")
+  )
+  
+  sim_1 %<>% run("one_simulation")
+  
+  sim_1$constants <- NULL # !!!!! TEMP FIX
+  summ <- sim_1 %>% summary() %>% rename("power"=`mean_reject`)
+
   # Visualize results
   ggplot(summ, aes(x=n, y=power, color=factor(test))) +
     geom_line() + geom_point() +
-    # facet_wrap(~true_density, ncol=3) +
     facet_wrap(~true_density, ncol=3, scales="free_y") +
     labs(
       x = "sample size", y = "Power", color = "Test type",
-      title = "Power of various tests for monotone density testing (10,000 replicates per level)"
+      title = paste0("Power of tests for constant vs. monotone density (10,000",
+                     " sims per level)")
     )
+  
+}
 
+
+
+#######################################################################.
+##### MAIN: Sim #2: compare different weights for CumlIncr method #####
+#######################################################################.
+
+if (run_main) {
+  
+  sim_2 <- sim
+  
+  sim_2 %<>% set_config(num_sim=10000)
+  
+  sim_2 %<>% set_levels(
+    n = c(10,30,50),
+    test = list(
+      "0.0" = list(type="CumlIncr", params=list(delta=1/3,wts=c(1.0,0.0))),
+      "0.1" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.9,0.1))),
+      "0.2" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.8,0.2))),
+      "0.3" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.7,0.3))),
+      "0.4" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.6,0.4))),
+      "0.5" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.5,0.5))),
+      "0.6" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.4,0.6))),
+      "0.7" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.3,0.7))),
+      "0.8" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.2,0.8))),
+      "0.9" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.1,0.9))),
+      "1.0" = list(type="CumlIncr", params=list(delta=1/3,wts=c(0.0,1.0)))
+    ),
+    true_density = c("f(x)=1", "f(x)=2x", "f(x)=ke^x")
+  )
+  
+  sim_2 %<>% run("one_simulation")
+  
+  sim_2$constants <- NULL # !!!!! TEMP FIX
+  summ <- sim_2 %>% summary() %>% rename("power"=`mean_reject`)
+
+  # Visualize results
+  ggplot(summ, aes(x=test, y=power, group=(n), color=factor(n))) +
+    geom_line() + geom_point() +
+    facet_wrap(~true_density, ncol=3, scales="free_y") +
+    labs(
+      x = "w_2 (weight)", y = "Power", color = "n", group = "n",
+      title = paste0("Power of CumlIncr test for constant vs. monotone density",
+                     " (10,000 sims per level)")
+    )
+  
+}
+
+
+
+############################################################################.
+##### MAIN: Sim #3: compare different delta values for CumlIncr method #####
+############################################################################.
+
+if (run_main) {
+  
+  sim_3 <- sim
+  
+  sim_3 %<>% set_config(num_sim=10000)
+  
+  sim_3 %<>% set_levels(
+    n = c(10,30,50),
+    test = list(
+      "1/2" = list(type="CumlIncr", params=list(delta=1/2,wts="equal")),
+      "1/3" = list(type="CumlIncr", params=list(delta=1/3,wts="equal")),
+      "1/4" = list(type="CumlIncr", params=list(delta=1/4,wts="equal")),
+      "1/5" = list(type="CumlIncr", params=list(delta=1/5,wts="equal")),
+      "1/6" = list(type="CumlIncr", params=list(delta=1/6,wts="equal")),
+      "1/7" = list(type="CumlIncr", params=list(delta=1/7,wts="equal")),
+      "1/8" = list(type="CumlIncr", params=list(delta=1/8,wts="equal")),
+      "1/9" = list(type="CumlIncr", params=list(delta=1/9,wts="equal")),
+      "1/10" = list(type="CumlIncr", params=list(delta=1/10,wts="equal"))
+      # "1/4" = list(type="CumlIncr", params=list(delta=1/4,wts="equal")),
+      # "1/8" = list(type="CumlIncr", params=list(delta=1/8,wts="equal")),
+      # "1/16" = list(type="CumlIncr", params=list(delta=1/16,wts="equal")),
+      # "1/32" = list(type="CumlIncr", params=list(delta=1/32,wts="equal")),
+      # "1/64" = list(type="CumlIncr", params=list(delta=1/64,wts="equal")),
+      # "1/128" = list(type="CumlIncr", params=list(delta=1/128,wts="equal"))
+    ),
+    true_density = c("f(x)=1", "f(x)=2x", "f(x)=ke^x")
+  )
+  
+  sim_3 %<>% run("one_simulation")
+  
+  sim_3$constants <- NULL # !!!!! TEMP FIX
+  summ <- sim_3 %>% summary() %>% rename("power"=`mean_reject`)
+  summ$test <- factor(summ$test, levels=c("1/2","1/3","1/4","1/5","1/6","1/7",
+                                          "1/8","1/9","1/10"))
+  # summ$test <- factor(summ$test, levels=c("1/2","1/4","1/8","1/16","1/32",
+  #                                         "1/64","1/128"))
+  
+  # Visualize results
+  ggplot(summ, aes(x=test, y=power, group=(n), color=factor(n))) +
+    geom_line() + geom_point() +
+    facet_wrap(~true_density, ncol=3, scales="free_y") +
+    labs(
+      x = "delta", y = "Power", color = "n", group = "n",
+      title = paste0("Power of CumlIncr test for constant vs. monotone density",
+                     " (10,000 sims per level)")
+    )
+  
 }
